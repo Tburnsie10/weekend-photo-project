7a1a563fa7ab508ee0b356f1d3015fc9a17a3cad
Tyler Burns <Tburnsie10@gmail.com>
2023-06-20 14:59:17 -0400

    finished post componenet

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,5 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 1 },
     { id: 2, path: 'images/cheetah.jpeg', description: 'Photo of a cheetah on the savanaha', likes: 5 },
     { id: 3, path: 'images/elephant.jpeg', description: 'Photo of a Bull Elephant walking.', likes: 10 },
     { id: 4, path: 'images/fox.jpeg', description: 'Photo of a fox taken in the forest', likes: 15 }
--- src/post.jsx
+++ src/post.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import './App.css';
+
 
 
 
 export default function Post({post}){
     return(
-        <div> {post.id} {post.path} {post.description} {post.likes} </div>
+        <div id={post.id}>
+             <div id={'image-' + post.id} >
+                <img className='images' src={post.path} />
+                <p>Heyyy</p>
+            </div>
+             <h4>Like</h4>
+             {
+                post.likes == 1 ? 
+                <h4> {post.likes} person has liked this! </h4>:
+                <h4> {post.likes} people have liked this! </h4>
+             }
+
+             {post.description}</div>
     )
 }
